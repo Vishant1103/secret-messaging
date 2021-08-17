945e6526af7bba881016616cfa27890742664df3
Vishant1103 <67213451+Vishant1103@users.noreply.github.com>
2021-08-17 20:34:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const { hash } = window.location;
+    const decrypted = atob(hash.replace('#', ''));
+    
+    if(decrypted){
+        document.querySelector('#message-form').classList.add('hide');
+        document.querySelector('#show-message').classList.remove('hide');
+
+        document.querySelector('h1').innerHTML = decrypted;
+    }
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+});
